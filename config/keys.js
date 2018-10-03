1ef4717ba6d335f95b50d4e1e388d1590aefa38a
Charpell <charpellumeh@gmail.com>
2018-10-03 15:16:39 +0100

    Prepare for heroku deployment

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else if (process.env.NODE_ENV === "test") {
+  module.exports = require("./keys_test");
+} else {
+  module.exports = require("./keys_dev");
+}
