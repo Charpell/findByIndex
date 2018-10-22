e2e162ffd0563e4f7e892fd352415bd4eb6be95d
Charpell <charpellumeh@gmail.com>
2018-10-22 20:20:21 +0100

    feat(user): activate vendor role
    - change role from user to vendor
    
    [Delivers]

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const { catchErrors } = require("../helpers/errorhandlers");
-const { createUser, loginUser } = require("../controllers/userController");
+const {
+  createUser,
+  loginUser,
+  activateVendorMode
+} = require("../controllers/userController");
+const { authenticate } = require("../middleware");
 
 router.post("/", catchErrors(createUser));
 router.post("/login", catchErrors(loginUser));
+router.patch("/vendor", authenticate, catchErrors(activateVendorMode));
 
 module.exports = router;
