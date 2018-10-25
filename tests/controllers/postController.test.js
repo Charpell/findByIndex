21a2ffa9a2901d48b8a3f0c59c1dc2fb87a23a20
Charpell <charpellumeh@gmail.com>
2018-10-25 08:15:58 +0100

    feat(post): get posts
    - users should see all posts
    - users should see one post
    - write test to sell posts
    
    [Delivers]

--- tests/controllers/postController.test.js
+++ tests/controllers/postController.test.js
@@ -55,3 +55,33 @@ describe("Create a Post", () => {
     expect(res.status).toBe(201);
   });
 });
+
+describe("Get posts", () => {
+  beforeEach(async () => {
+    await Post.insertMany(posts);
+  });
+
+  it("should return 404 if no post found", async () => {
+    const res = await request(app).get("/api/posts/5bc259266d2022c8e1fd4e39");
+
+    expect(res.status).toBe(404);
+  });
+
+  it("should return 404 for invalid ID", async () => {
+    const res = await request(app).get("/api/posts/333");
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should get one post", async () => {
+    const res = await request(app).get(`/api/posts/${posts[0]._id}`);
+
+    expect(res.status).toBe(200);
+  });
+
+  it("should get all the meals", async () => {
+    const res = await request(app).get("/api/posts");
+
+    expect(res.status).toBe(200);
+  });
+});
