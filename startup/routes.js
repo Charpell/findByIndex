cbad9edd18bde6218b4ee190bf8e232c4bf93899
Charpell <charpellumeh@gmail.com>
2018-10-28 07:57:13 +0100

    feat(vendor): get vendor's meals
    - create controller to get vendors meal
    - edit getMeals controller to return vendor
    
    [Delivers]

--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const users = require("../routes/userRoute");
 const meals = require("../routes/mealRoute");
 const posts = require("../routes/postRoute");
+const vendors = require("../routes/vendorRoutes");
 const { serverError, validationError } = require("../middleware/error");
 
 module.exports = function(app) {
@@ -9,6 +10,7 @@ module.exports = function(app) {
   app.use("/api/users", users);
   app.use("/api/meals", meals);
   app.use("/api/posts", posts);
+  app.use("/api/vendors", vendors);
   app.use(validationError);
   app.use(serverError);
 };
