cfae7cd8fea5ce0c9616dd3842f3b8558c7cc92f
Charpell <charpellumeh@gmail.com>
2018-10-28 22:13:44 +0100

    feat(chat): implement chat feature
    - create model for chat
    - abstract chat logic into chat folder
    - write index html file
    
    [Delivers]

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 const http = require("http");
-const socketIO = require("socket.io");
-
-const publicPath = path.join(__dirname, "public");
 
 const expressApp = express();
 const app = http.createServer(expressApp);
-const io = socketIO(app);
-
-expressApp.use(express.static(publicPath));
-
-io.on("connection", socket => {
-  console.log("New user connected");
-
-  socket.on("createMessage", message => {
-    console.log("createMessage", message);
-
-    io.emit("newMessage", {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User was disconnected");
-  });
-});
 
 require("./startup/routes")(expressApp);
 require("./startup/db")();
 require("./startup/config")();
+require("./chat")(app);
 
 module.exports = app;
