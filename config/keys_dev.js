f58ce2dad361402e5e1183fc996ef52f059e7106
Charpell <charpellumeh@gmail.com>
2018-10-29 19:56:38 +0100

    feat(notification): setup nodemailer for email notification
    - install nodemailer
    - setup config for naodemailer
    - send email to a newly registered user
    
    [Delivers]

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,7 @@
 module.exports = {
   jwtPrivateKey: process.env.JWT_PRIVATE_KEY,
   mongoURI: process.env.MONGO_URI,
-  port: process.env.DEV_PORT
+  port: process.env.DEV_PORT,
+  emailAddress: process.env.EMAIL_ADDRESS,
+  password: process.env.PASSWORD
 };
