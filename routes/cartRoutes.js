d1c1e89c6f16b511523bee1afd9c188c00f8d338
Charpell <charpellumeh@gmail.com>
2018-10-30 07:47:32 +0100

    feat(cart): remove items from cart
    - create removeCart controller
    - create remveCart route
    
    [Delivers]

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const { authenticate } = require("../middleware");
 const { catchErrors } = require("../helpers/errorhandlers");
 
-const { createCart, getCarts } = require("../controllers/cartController");
+const {
+  createCart,
+  getCarts,
+  removeCart
+} = require("../controllers/cartController");
 
 router.post("/", authenticate, catchErrors(createCart));
 router.get("/", authenticate, catchErrors(getCarts));
+router.delete("/:id", authenticate, catchErrors(removeCart));
 
 module.exports = router;
