f860c7da7e8adf7495580a74c6fafe9e6585e147
Charpell <charpellumeh@gmail.com>
2018-11-02 08:59:20 +0100

    feat(bookedMeal): vendor accepts booking
    - vendor changes the meal status to from pending to progress
    - notify the customer that the meal has been accepted and is in progress
    
    [Delivers]

--- controllers/shopVendorController.js
+++ controllers/shopVendorController.js
@@ -1,4 +1,7 @@
+const mongoose = require("mongoose");
+
 const { Shopping } = require("../models/shoppingModel");
+const nodemailer = require("../helpers/nodemailer");
 
 const getAllBookedMeals = async (req, res) => {
   const mealItems = await Shopping.find({
@@ -13,6 +16,37 @@ const getAllBookedMeals = async (req, res) => {
   res.status(200).json(mealItems);
 };
 
+const vendorAcceptsBooking = async (req, res) => {
+  const isValid = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!isValid) return res.status(400).json({ error: "Invalid ID" });
+
+  let item = await Shopping.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      "vendor._id": req.user._id
+    },
+    {
+      $set: {
+        status: "progress"
+      }
+    },
+    {
+      new: true
+    }
+  );
+
+  if (!item) return res.status(404).json({ response: "Not Found" });
+
+  res.status(200).json({
+    message:
+      "You have accepted the booked meal. We will notify the customer that the meal is in progress",
+    response: item
+  });
+
+  nodemailer(item.customer.email);
+};
+
 module.exports = {
-  getAllBookedMeals
+  getAllBookedMeals,
+  vendorAcceptsBooking
 };
