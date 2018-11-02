52a1094cb16b5b82838d8f30c89163d1b8c60ad6
Charpell <charpellumeh@gmail.com>
2018-11-02 18:28:38 +0100

    chore(cors): habdling cors
    - create cors middleware to allow cross origin
    
    [Delivers]

--- startup/routes.js
+++ startup/routes.js
@@ -5,9 +5,11 @@ const posts = require("../routes/postRoute");
 const vendors = require("../routes/vendorRoutes");
 const shop = require("../routes/shoppingRoutes");
 const { serverError, validationError } = require("../middleware/error");
+const cors = require("../middleware/cors");
 
 module.exports = function(app) {
   app.use(express.json());
+  app.use(cors);
   app.use("/api/users", users);
   app.use("/api/meals", meals);
   app.use("/api/posts", posts);
